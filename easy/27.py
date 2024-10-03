dd0c278d1b47c6108387d57b5e2f8a3a66c7f318
Sydney <68250402+cecelot@users.noreply.github.com>
2024-10-02 22:53:14 -0400

    feat(easy): 27.py

---
+++ easy/27.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        startIndex = 0
+        searchedNums = 0
+        while searchedNums < len(nums):
+            checkNum = nums[startIndex]
+            searchedNums += 1
+            if checkNum == val:
+                for i in range(startIndex + 1, len(nums)):
+                    nums[i - 1] = nums[i]
+                nums[-1] = checkNum
+            else:
+                startIndex += 1
+        print(sorted(nums))
+        return startIndex
+
+
+print(Solution().removeElement([3, 2, 2, 3], 3))
+print(Solution().removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
